f4421cb629e234355d7760d11cb5d15a799f5a32
Jay Joo <jayjoo0727@gmail.com>
2025-08-05 05:11:57 -0700

    fixed blocking issue where TTS prevented keyboard interrupt

--- src/TTS.py
+++ src/TTS.py
@@ -3,8 +3,11 @@ import time
 import numpy as np
 import logging
 from pathlib import Path
+from typing import Tuple
+
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 from StyleTTS import msinference
 from StyleTTS.app import synthesize
@@ -23,9 +26,38 @@ class TTS:
         )
 
         self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(
-            format=pyaudio.paInt16, channels=1, rate=24000, output=True
-        )
+        self.stream = None
+
+        self._audio_data = b""
+        self._audio_position = 0
+
+    def _audio_callback(
+        self, in_data, frame_count: int, time_info, status
+    ) -> Tuple[bytes, int]:
+        """Callback function that the PyAudio thread calls to iteratively get more audio data.
+
+        Args:
+            frame_count: The number of frames of audio data the stream needs
+
+        Returns:
+            Tuple[bytes, int]: Tuple containing an audio chunk and a flag indicating if there are still chunks left to play.
+        """
+        bytes_to_read = frame_count * self.p.get_sample_size(pyaudio.paInt16)
+
+        chunk = self._audio_data[
+            self._audio_position : self._audio_position + bytes_to_read
+        ]
+
+        self._audio_position += bytes_to_read
+
+        if len(chunk) < bytes_to_read:
+            padding_size = bytes_to_read - len(chunk)
+            chunk += b"\x00" * padding_size
+            flag = pyaudio.paComplete
+        else:
+            flag = pyaudio.paContinue
+
+        return (chunk, flag)
 
     def text_to_speech(self, text: str) -> None:
         """Runs StyleTTS inference on provided text.
@@ -41,11 +73,28 @@ class TTS:
 
         wav_data = outputs[1]
         wav_data = wav_data / np.max(np.abs(wav_data))
-        audio_int16 = (wav_data * 32767).astype(np.int16)
-        self.stream.write(audio_int16.tobytes())
+        self._audio_data = (wav_data * 32767).astype(np.int16).tobytes()
+        self._audio_position = 0
+
+        self.stream = self.p.open(
+            format=pyaudio.paInt16,
+            channels=1,
+            rate=24000,
+            output=True,
+            stream_callback=self._audio_callback,
+        )
+        logger.info("Playing audio...")
+        try:
+            while self.stream.is_active():
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            logger.info("Playback interrupted by user.")
+        finally:
+            if self.stream:
+                self.stream.stop_stream()
+                self.stream.close()
+                self.stream = None
 
     def close_stream(self) -> None:
         """Closes Pyaudio stream."""
-        self.stream.stop_stream()
-        self.stream.close()
         self.p.terminate()
